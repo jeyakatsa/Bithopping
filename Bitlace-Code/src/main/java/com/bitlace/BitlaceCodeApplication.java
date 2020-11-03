18bdd81d326c76f4905d220ee4cc6dabd8864d04
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-11-02 18:00:30 -0800

    Started Code

---
+++ Bitlace-Code/src/main/java/com/bitlace/BitlaceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.bitlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlaceCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitlaceCodeApplication.class, args);
+	}
+
+}
